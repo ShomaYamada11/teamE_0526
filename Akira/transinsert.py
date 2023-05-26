9a28503b40c373a4ac088722f468c2f311bde358
AkiraTasuku <sato.akira.nt@mynavi.jp>
2023-05-26 06:37:38 +0000

    交通費精算db

--- Akira/transinsert.py
+++ Akira/transinsert.py
@@ -3,14 +3,18 @@ from sqlalchemy import Column,Integer,VARCHAR,DECIMAL,Date
 from database import Base
 from database import ENGINE
 from database import session
+from datetime import date,datetime
 
 
 
-class Station(Base):
+class Transport(Base):
     __tablename__='transport'
-    date = Column('date',Date,primary_key=True)
-    seq = Column("seq",Integer)
+    date = Column('date',Date)
+    seq = Column("seq",Integer,primary_key=True)
     departure = Column("departuer",VARCHAR(20))
+    arrival = Column("arrival", VARCHAR(20))
+    via = Column("via",VARCHAR(40) )
+    amount = Column("amount", Integer)
     
 def main(args):
 
@@ -21,17 +25,34 @@ if __name__=="__main__":
     main(sys.argv)
 
     args = sys.argv
-    start_station_name = args[1]
-    finish_station_name = args[2]
+    input_stationdata = list(map(str,args[1:]))
 
-    station_name_list = session.query(Station).all()
-    for list in station_name_list:
-        if list.name == start_station_name:
-            start_station_kiro = list.kilo
+    dt = datetime.strptime(input_stationdata[0], "%Y%m%d")
 
-        if list.name == finish_station_name:
-            finish_station_kiro = list.kilo
 
-    print(abs(start_station_kiro - finish_station_kiro))
+
+    # station_name_list = session.query(Transport).all()
+    # for list in station_name_list:
+    #     if list.seq == int(input_stationdata[1]):
+    #         print("error:交通費精算テーブルにデータを登録できませんでした")
+    #         break
+    
+
+    try:
+        transport = Transport(
+            date = dt,
+            seq = input_stationdata[1],
+            departure = input_stationdata[2],
+            arrival = input_stationdata[3],
+            via = input_stationdata[4],
+            amount = input_stationdata[5],
+        )
+        session.add(transport)
+        session.commit()
+        print("交通費テーブルにデータを登録できました")
+    except:
+        print("error:交通費精算テーブルにデータを登録できませんでした")
+
+
 
 
