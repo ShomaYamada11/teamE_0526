93927b08abee8c9ed6801976543864b0b30e3cb0
SuzukiRika00 <suzuki.rika.zz@mynavi.jp>
2023-05-29 01:06:06 +0000

    add

--- SuzukiRika00/distancedb.py
+++ SuzukiRika00/distancedb.py
@@ -6,7 +6,9 @@ args = sys.argv
 departure_station = (args[1])
 arrival_station = (args[2])
 
-stations=session.query(Stations).filter(Stations.name.in_([input_from, input_to])).all()
+stations=session.query(Stations).filter(Stations.name.in_([departure_station, arrival_station])).all()
+#ここまでやった途中
+#distance = map[args[2]] - map[args[1]]
+distance = stations[0].kilo - stations[1].kilo
 
-distance = map[args[2]] - map[args[1]]
 print(abs(round(distance, 2)), end="")
