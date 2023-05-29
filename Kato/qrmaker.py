13942bfa0fba06f6bc24830447f4b69de8190c9c
katoyuna-mynavi <kato.yuna.oj@mynavi.jp>
2023-05-29 05:41:59 +0000

    QRコード作成(コメント追加)

--- Kato/qrmaker.py
+++ Kato/qrmaker.py
@@ -4,12 +4,11 @@ args = sys.argv
 import qrcode
 import os
 
-def make_qr(seq, url):
-    path = os.path.join("..", "..", "Python", "output", str(seq) + ".png") 
-    img = qrcode.make(url)
-    img.save(path)
+def make_qr(seq, url):      # QRコードを作成し保存する関数を定義
+    path = os.path.join("..", "..", "Python", "output", str(seq) + ".png")      # 保存先のファイル名を生成
+    img = qrcode.make(url)      # QRコードを作成
+    img.save(path)      # QRコードを保存
 
-with open(args[1], mode="r", encoding="utf-8") as f:
-    for i, line in enumerate(f):
-        print(line)
-        make_qr(i, line)
+with open(args[1], mode="r", encoding="utf-8") as f:    # urlのリストがあるファイルを読み込みモードで開く
+    for i, line in enumerate(f):    # 1行ずつ読み込む
+        make_qr(i, line)    # QRコードを作成する関数を呼び出す
