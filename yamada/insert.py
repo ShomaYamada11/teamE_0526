361c30c6674b032512a9d978712819b198ed9f51
ShomaYamada11 <yamada.shoma.db@mynavi.jp>
2023-05-29 02:03:48 +0000

    transinsert.pyを追加

--- yamada/insert.py
+++ yamada/insert.py
@@ -1,52 +1,51 @@
-from datetime import date
 from database import session #1.データベースへの接続
 from stationtable import Stations #2.テーブル定義
 
 #登録するデータの編集
-holiday = Stations(
+station1 = Stations(
     seq = 1,
     name = "東京",
     kilo = 0.00
 )
-session.add(holiday)
+session.add(station1)
 session.commit()
 
-holiday = Stations(
+station2 = Stations(
     seq =2,
     name = "品川",
     kilo = 6.78
 )
-session.add(holiday)
+session.add(station2)
 session.commit()
 
-holiday = Stations(
+station3 = Stations(
     seq = 3,
     name = "新横浜",
     kilo = 25.54
 )
-session.add(holiday)
+session.add(station3)
 session.commit()
 
-holiday = Stations(
+station4 = Stations(
     seq = 4,
     name = "名古屋",
     kilo = 342.02
 )
-session.add(holiday)
+session.add(station4)
 session.commit()
 
-holiday = Stations(
+station5 = Stations(
     seq = 5,
     name = "京都",
     kilo = 476.31
 )
-session.add(holiday)
+session.add(station5)
 session.commit()
 
-holiday = Stations(
+station6 = Stations(
     seq = 6,
     name = "新大阪",
     kilo = 515.35
 )
-session.add(holiday)
+session.add(station6)
 session.commit()
---
+++ yamada/transinsert.py
@@ -0,0 +1,68 @@
+import sys
+from sqlalchemy import Column, Integer, Date, VARCHAR
+from database import Base
+from database import ENGINE
+from database import session
+from datetime import date
+
+#テーブル:Transportの定義
+class Transport(Base):
+    __tablename__ = 'transport'
+    date = Column('date', Date, primary_key = True)
+    seq = Column('seq', Integer, primary_key = True)
+    departure = Column('departure', VARCHAR(20))
+    arrival = Column('arrival', VARCHAR(20))
+    via = Column('via', VARCHAR(40))
+    amount = Column('amount', Integer)
+
+def main(args):
+    """メイン関数"""
+    Base.metadata.create_all(bind=ENGINE)
+
+if __name__ == "__main__":
+    main(sys.argv)
+
+args = sys.argv
+
+#引数を変数に代入
+today = args[1]
+#年月日をスライス
+year = today[:4]
+month = today[4:6]
+day = today[6:]
+
+num = int(args[2])
+departure_station = args[3]
+arrival_station = args[4]
+via_line = args[5]
+money = int(args[6])
+
+#既に同じ日、連番のものがあるかチェック
+existing_data = 0
+transport_list = session.query(Transport).all()
+for transports in transport_list:
+    if transports.date == date(int(year),int(month),int(day)) and transports.seq == num:
+        existing_data = 1
+        break
+
+#ある場合とない場合のパターン分け
+if existing_data != 0:
+    print("error:交通費精算テーブルにデータを登録できませんでした")
+
+else:
+    #登録するデータ
+    data = Transport(
+        date = date(int(year),int(month),int(day)),
+        seq = num,
+        departure = departure_station,
+        arrival = arrival_station,
+        via = via_line,
+        amount = money
+    )
+
+    #データ登録
+    session.add(data)
+    session.commit()
+
+    #データ登録完了を通知
+    print("交通費精算テーブルにデータを登録しました")
